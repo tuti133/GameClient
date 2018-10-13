a70ace9e89d0357e15450ebfbd762637c1b07305
ductv <ductranplay@gmail.com>
2018-10-13 18:52:57 +0700

    demo game play with questions

---
+++ src/gameclient/model/response/GetRandomQuestionResponseDto.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gameclient.model.response;
+
+import gameclient.model.Question;
+import gameclient.model.ResponseDto;
+import java.util.List;
+
+/**
+ *
+ * @author Nobody
+ */
+public class GetRandomQuestionResponseDto extends ResponseDto{
+    List<Question> questions;
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<Question> questions) {
+        this.questions = questions;
+    }
+    
+}
