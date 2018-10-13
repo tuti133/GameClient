a0e5b6ef5b509f52d4934c081b7eaa858635614a
Hoàng Minh Hưng <hoang@DESKTOP-O931NNJ>
2018-10-13 11:59:52 +0700

    Time Game

--- src/gameclient/util/Constant.java
+++ src/gameclient/util/Constant.java
@@ -10,11 +10,13 @@ package gameclient.util;
  * @author Admin
  */
 public interface Constant {
+
     String BASE_URL = "http://localhost:8080/api/";
     String ERROR = "1";
     String ONLINE_STATUS = "0";
     String OFFLINE_STATUS = "1";
     String AVAILABLE_STATUS = "2";
     String BUSY_STATUS = "3";
-    
+
+    int TIME_PLAY = 180;
 }
---
+++ src/gameclient/util/TimeMatch.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gameclient.util;
+
+import gameclient.view.GamePlayFrm;
+
+/**
+ *
+ * @author hoang
+ */
+public class TimeMatch implements Runnable {
+
+    private GamePlayFrm gamePlayFrm;
+
+    public static void main(String[] args) {
+        GamePlayFrm gamePlayFrm = new GamePlayFrm();
+        new Runnable() {
+            @Override
+            public void run() {
+                gamePlayFrm.setVisible(true);
+            }
+        }.run();
+        Thread time = new Thread(new TimeMatch(gamePlayFrm));
+        time.start();
+    }
+
+    public TimeMatch(GamePlayFrm gamePlayFrm) {
+        this.gamePlayFrm = gamePlayFrm;
+    }
+
+    public TimeMatch() {
+    }
+
+    @Override
+    public void run() {
+        runTime();
+    }
+
+    public void runTime() {
+        int time = Constant.TIME_PLAY;
+        while (time > 0) {
+            time--;
+            gamePlayFrm.setLbTimeLeft(showTime(time));
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+
+            }
+        }
+    }
+
+    public static String showTime(int time) {
+        int min = time / 60;
+        int sec = time % 60;
+        if (sec < 10) {
+            return "0" + min + ":" + "0" + sec;
+        } else {
+            return "0" + min + ":" + sec;
+        }
+    }
+
+}
